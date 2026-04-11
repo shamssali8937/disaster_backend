8a356d48b6ae40b6a800e8c62ae4d5b739e62462
shamssali8937 <sa4857433@gmail.com>
2026-04-11 17:22:48 +0500

    created the endpoint

--- app/services/firestore_service.py
+++ app/services/firestore_service.py
@@ -1,32 +1,25 @@
-import uuid
-from firebase_admin import firestore
-from app.core.config import db, bucket
+# import uuid
 
-class FirestoreService:
+# class DBService:
 
-    def create_user(self, uid, email, role):
-        db.collection("Users").document(uid).set({
-            "email": email,
-            "role": role,
-            "created_at": firestore.SERVER_TIMESTAMP
-        })
+#     def upload_file(self, file_bytes, filename, content_type):
+#         # 🔥 Simplified (replace with Firebase later)
+#         return f"https://fake-storage.com/{filename}"
+
+#     def create_report(self, user_id, media_url, disaster, severity):
+#         # 🔥 Fake DB
+#         return str(uuid.uuid4())
+
+
+# db_service = DBService()
+
+class DBService:
 
     def upload_file(self, file_bytes, filename, content_type):
-        ext = filename.split('.')[-1]
-        blob = bucket.blob(f"disaster_media/{uuid.uuid4()}.{ext}")
-        blob.upload_from_string(file_bytes, content_type=content_type)
-        blob.make_public()
-        return blob.public_url
+        return "No Storage Used"
 
-    def create_report(self, uid, media_url, disaster, severity):
-        doc_ref = db.collection("Reports").add({
-            "reporter_id": uid,
-            "media_url": media_url,
-            "disaster_type": disaster,
-            "severity": severity,
-            "status": "Pending",
-            "created_at": firestore.SERVER_TIMESTAMP
-        })
-        return doc_ref[1].id
+    def create_report(self, user_id, media_url, disaster, severity):
+        return "report_123"
 
-db_service = FirestoreService()
+
+db_service = DBService()
